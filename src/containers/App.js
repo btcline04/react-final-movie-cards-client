a5efd40deb0e14d155801434ab86c73ba85fb370
btcline04 <btcline04@gmail.com>
2018-09-24 14:17:04 -0400

    Set up Movies component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Movies from './Movies.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        App Container
+        <Movies />
       </div>
     );
   }
