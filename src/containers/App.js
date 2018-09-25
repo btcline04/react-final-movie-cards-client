b63bd95dfcbde035edc5c5578693da23634abb8c
btcline04 <btcline04@gmail.com>
2018-09-25 11:10:46 -0400

    Create .env file and change fetch request to use API key

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
-import Movies from './Movies.js';
+import Movies from './Movies';
+
+const API_URL = process.env.REACT_APP_API_URL;
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      movies: []
+    }
+  }
+
+  componentDidMount() {
+    fetch(`${API_URL}/movies`)
+      .then(resp => resp.json())
+      .then(movies => this.setState({ movies }))
+  }
+
   render() {
     return (
       <div className="App">
-        <Movies />
+        <Movies movies={this.state.movies}/>
       </div>
     );
   }
--- src/containers/Movies.js
+++ src/containers/Movies.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
-const Movies = () => {
+const Movies = (props) => {
   return (
     <div>
-      <h3>Movies Component</h3>
+      <h1>Movies</h1>
+      {props.movies.map(movie => 
+      <div key={movie.id}>
+        <h3>{movie.title}</h3>
+        <p>{movie.plot_summary}</p>
+        <img src={movie.img_url} alt={movie.title}/>
+        <p>Run time: {movie.run_time} minutes</p>
+      </div>
+      )}
     </div>
   )
 }
